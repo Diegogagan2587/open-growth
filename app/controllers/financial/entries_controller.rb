f64df36f03458b207e46daff6cd642de5ba671a7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-05-22 11:57:57 -0600

    feat(Financial::EntriesController): add :edit and :update actions to befere_acction execution list for methods set_financial_entry and laod_form_collections

--- app/controllers/financial/entries_controller.rb
+++ app/controllers/financial/entries_controller.rb
@@ -2,8 +2,8 @@ module Financial
   class EntriesController < ApplicationController
     include Financial::AccountReferenceFiltering
 
-    before_action :set_financial_entry, only: [ :show, :destroy ]
-    before_action :load_form_collections, only: [ :new, :create ]
+    before_action :set_financial_entry, only: [ :show, :edit, :update, :destroy ]
+    before_action :load_form_collections, only: [ :new, :create, :edit, :update ]
     before_action :load_account_filter_options, only: [ :index ]
 
     def index
