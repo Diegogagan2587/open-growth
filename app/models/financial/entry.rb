cd05620450f4147e2fdb1ed59a61aedca033580a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-05-31 20:32:47 -0600

    feat(Finance::Entry): validates presence of :category & budget_period only when Entry is outflow or liability charge

--- app/models/financial/entry.rb
+++ app/models/financial/entry.rb
@@ -23,6 +23,8 @@ class Financial::Entry < ApplicationRecord
   validates :entry_date, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :description, presence: true
+  validates :category, presence: true, if: :classification_required?
+  validates :budget_period, presence: true, if: :classification_required?
 
   validate :required_links_by_type
   validate :associations_belong_to_same_account
