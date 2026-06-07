d4e0b74cf350a2f92824e129d0dbcec662bf2c07
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-06-06 19:55:11 -0600

    feat(Financial::EntriesController#index): filter by entry_date, category or description if date_from, date_to, category_id or :q params are present

--- app/controllers/financial/entries_controller.rb
+++ app/controllers/financial/entries_controller.rb
@@ -10,6 +10,10 @@ module Financial
     def index
       @financial_entries = Financial::Entry.for_account(Current.account).by_date
       @financial_entries = apply_account_ref_filter(@financial_entries)
+      @financial_entries = @financial_entries.where("entry_date >= ?", params[:date_from]) if params[:date_from].present?
+      @financial_entries = @financial_entries.where("entry_date <= ?", params[:date_to]) if params[:date_to].present?
+      @financial_entries = @financial_entries.where(category_id: params[:category_id]) if params[:category_id].present?
+      @financial_entries = @financial_entries.where("description ILIKE ?", "%#{params[:q]}%") if params[:q].present?
       @selected_account_ref = selected_account_ref
     end
 
