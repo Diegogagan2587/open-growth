085f02e5354cbf7206526a991a74fbdc30281329
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-06-28 12:33:50 -0600

    fix(Career::JobApplicationsController#set_job_applicaiton): removes not required eager laading on method invoked before action

--- app/controllers/career/job_applications_controller.rb
+++ app/controllers/career/job_applications_controller.rb
@@ -166,7 +166,7 @@ module Career
     private
 
     def set_job_application
-      @job_application = Career::JobApplication.for_account(Current.account).includes(:company).find(params[:id])
+      @job_application = Career::JobApplication.for_account(Current.account).find(params[:id])
     end
 
     def job_application_params
