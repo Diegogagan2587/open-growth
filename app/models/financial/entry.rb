a522330fff2c531ffa42616b175c3dca6d731ea4
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-06-29 19:02:06 -0600

    feat(Financial::Entry): define constant EXPENSE_ENTRY_TYPES as outflow and liabitly_charge types of entry_type

--- app/models/financial/entry.rb
+++ app/models/financial/entry.rb
@@ -1,7 +1,8 @@
 class Financial::Entry < ApplicationRecord
   self.table_name = "financial_entries"
 
-  ENTRY_TYPES = %w[inflow outflow transfer liability_charge liability_payment loan_disbursement adjustment].freeze
+  ENTRY_TYPES         = %w[inflow outflow transfer liability_charge liability_payment loan_disbursement adjustment].freeze
+  EXPENSE_ENTRY_TYPES = %w[outflow liability_charge].freeze
 
   belongs_to :account, class_name: "::Account"
   belongs_to :category, optional: true
