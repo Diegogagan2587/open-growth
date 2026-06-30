8a7d9f3c562fdd58075d0817ad080068d857c4c2
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-06-29 19:49:37 -0600

    feat(Financial::EntriesController#new): set entry_type to outflow if requested paramteri is expense as id does not exist as Financial::entry::EXPENSE_ENTRY_TYPES

--- app/controllers/financial/entries_controller.rb
+++ app/controllers/financial/entries_controller.rb
@@ -25,6 +25,7 @@ module Financial
 
     def new
       @financial_entry = Financial::Entry.new(entry_date: Date.current)
+      @financial_entry.entry_type = "outflow" if params[:entry_type] == "expenses"
     end
 
     def create
