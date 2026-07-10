83ff68f77b40065c54dafcdc3329ae43a38ecd44
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-07-09 20:42:04 -0600

    feat(Financial::DirectExpensesController): set income event and load form collection before action to present the current income vente while adding a direct expense from whithing Income Event view

--- app/controllers/financial/direct_expenses_controller.rb
+++ app/controllers/financial/direct_expenses_controller.rb
@@ -1,2 +1,19 @@
 class Financial::DirectExpensesController < ApplicationController
+  before_action :set_income_event
+  before_action :load_form_collections, only: [ :new, :create ]
+
+  
+  private
+  
+  def set_income_event
+    @income_event = IncomeEvent.for_account(Current.account).find(params[:income_event_id])
+  end
+
+  
+  def load_form_collections
+    @categories = Category.for_account(Current.account).order(:name)
+    @budget_periods = BudgetPeriod.for_account(Current.account).order(start_date: :desc)
+    @financial_accounts = Financial::Asset.for_account(Current.account).active.order(:name)
+    @financial_liabilities = Financial::Liability.for_account(Current.account).active.order(:name)
+  end
 end
