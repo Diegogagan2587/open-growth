116b026cd38b5a2caa3dad1a327f9d4f15fbed1c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-07-10 08:24:40 -0600

    config(routes.rb): moves comments boilerplate and healthcheck on top of the file to avoide having htem between implementated resoruces for better readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker, defaults: { format: :js }
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
   root "dashboard#index"
   resource :session
   resource :registration, only: [ :new, :create ]
@@ -25,19 +38,6 @@ Rails.application.routes.draw do
   get "settings/finance/categories/new", to: redirect("/finance/categories/new"), as: :new_settings_finance_category
   get "settings/finance/categories/:id/edit", to: redirect { |params, _request| "/finance/categories/#{params[:id]}/edit" }, as: :edit_settings_finance_category
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker, defaults: { format: :js }
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  #
   resources :expense_templates
 
   get "finance", to: "finance#index", as: :finance
