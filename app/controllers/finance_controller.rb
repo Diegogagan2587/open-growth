34d398e30a65c15be3f43e14528db468a3ac797c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-07-19 18:32:56 -0600

    feat(finance): expose plan and loan summaries

--- app/controllers/finance_controller.rb
+++ app/controllers/finance_controller.rb
@@ -4,6 +4,7 @@ class FinanceController < ApplicationController
     @financial_account_count = Financial::Asset.for_account(Current.account).count
     @financial_liability_count = Financial::Liability.for_account(Current.account).count
     @financial_entry_count = Financial::Entry.for_account(Current.account).count
-    @loan_count = IncomeEvent.for_account(Current.account).loans.count
+    @plan_count = Financial::Plan.for_account(Current.account).count
+    @loan_count = Financial::Loan.where(account: Current.account).count
   end
 end
