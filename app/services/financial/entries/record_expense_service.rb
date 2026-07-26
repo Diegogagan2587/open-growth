118ba6c0c4692ab936b9d1ae3a182a5b5f0de4fe
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-07-26 15:50:07 -0600

    feat(Financial::Entries::RecordExpenseService): add attribute reader for entry_time

--- app/services/financial/entries/record_expense_service.rb
+++ app/services/financial/entries/record_expense_service.rb
@@ -38,7 +38,7 @@ module Financial
 
       private
 
-      attr_reader :account, :amount, :entry_date, :description, :category_id, :budget_period_id, :source_selection, :destination_selection, :income_event_id, :planned_expense_id
+      attr_reader :account, :amount, :entry_date, :entry_time, :description, :category_id, :budget_period_id, :source_selection, :destination_selection, :income_event_id, :planned_expense_id
 
       def base_attributes
         {
