5988f5e95ba36fbf4ec45c0230e98f324fab9dc4
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-07-26 14:10:35 -0600

    feat(Ui::SelectComponent): add searchable parameter on component  initialization to enable/idsable searchable ui/ux features

--- app/components/ui/select_component.rb
+++ app/components/ui/select_component.rb
@@ -3,7 +3,7 @@
 class Ui::SelectComponent < ViewComponent::Base
   BASE_CLASSES = "flex h-9 w-full rounded-md border border-input bg-background px-3 py-1 text-sm text-foreground shadow-xs transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive".freeze
 
-  def initialize(name:, options:, selected: nil, id: nil, disabled: false, required: false, aria_invalid: false, include_blank: nil, data: {}, css_class: nil)
+  def initialize(name:, options:, selected: nil, id: nil, disabled: false, required: false, aria_invalid: false, include_blank: nil, data: {}, css_class: nil, searchable: false)
     @name = name
     @options = options
     @selected = selected
@@ -14,6 +14,7 @@ class Ui::SelectComponent < ViewComponent::Base
     @include_blank = include_blank
     @data = data
     @extra_class = css_class
+    @searchable = searchable
   end
 
   def rendered_options
