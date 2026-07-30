8f0cc248cd9a359c79cc50c97c58319cf6225f8a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-07-30 17:05:48 -0600

    feat(Ui::InputComponent#initialize): initialize autocomplete bool option when avaible and pass it as html attributes for rendering

--- app/components/ui/input_component.rb
+++ app/components/ui/input_component.rb
@@ -3,7 +3,7 @@
 class Ui::InputComponent < ViewComponent::Base
   BASE_CLASSES = "flex h-9 w-full min-w-0 rounded-md border border-input bg-background px-3 py-1 text-sm text-foreground shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive".freeze
 
-  def initialize(name:, value: nil, id: nil, type: "text", placeholder: nil, disabled: false, required: false, aria_invalid: false, data: {}, css_class: nil, min: nil, max: nil, step: nil)
+  def initialize(name:, value: nil, id: nil, type: "text", placeholder: nil, disabled: false, required: false, aria_invalid: false, data: {}, css_class: nil, min: nil, max: nil, step: nil, autocomplete: nil)
     @name = name
     @value = value
     @id = id
@@ -17,6 +17,7 @@ class Ui::InputComponent < ViewComponent::Base
     @min = min
     @max = max
     @step = step
+    @autocomplete = autocomplete
   end
 
   def html_attributes
@@ -33,6 +34,7 @@ class Ui::InputComponent < ViewComponent::Base
       min: @min,
       max: @max,
       step: @step,
+      autocomplete: @autocomplete,
       class: [ BASE_CLASSES, @extra_class ].compact.join(" ")
     }.compact
   end
