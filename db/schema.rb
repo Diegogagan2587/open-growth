ebacb7d0d8329311aeea528634d998a5adafe2cd
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-07-30 21:49:31 -0600

    raisl db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_30_225000) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_30_233000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -590,6 +590,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_30_225000) do
     t.date "date_from", null: false
     t.date "date_to", null: false
     t.string "title", null: false
+    t.integer "turns_count", default: 0, null: false
     t.datetime "updated_at", null: false
     t.index ["account_id"], name: "index_reporting_conversations_on_account_id"
     t.index ["account_membership_id", "updated_at"], name: "idx_on_account_membership_id_updated_at_2ea686a3eb"
