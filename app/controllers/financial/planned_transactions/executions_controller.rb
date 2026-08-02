67cfdfd0ba18bae76ab3e7a2396c5de8043fd2b5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-08-01 23:31:08 -0600

    feat(Financial::PlannedTransactions::ExecutionsController): add new controller for handling creation and destoyment of actualy transactions derived from planned transacitons

---
+++ app/controllers/financial/planned_transactions/executions_controller.rb
@@ -0,0 +1,15 @@
+class Financial::PlannedTransactions::ExecutionsController < ApplicationController
+  def create
+    planned = Financial::PlannedTransaction.for_account(Current.account).find(params[:planned_transaction_id])
+    attrs = params.fetch(:planned_transaction, {}).permit(:amount, :transaction_date, :entry_date, :description, :category_id, :source_account_id, :destination_account_id).to_h.symbolize_keys
+    attrs[:transaction_date] ||= attrs.delete(:entry_date)
+    result = Financial::PlannedTransactions::Execution.create(planned_transaction: planned, attributes: attrs)
+    redirect_back fallback_location: finance_planned_transactions_path, notice: ("Transaction executed" if result.success?), alert: (result.error_message unless result.success?)
+  end
+
+  def destroy
+    planned = Financial::PlannedTransaction.for_account(Current.account).find(params[:planned_transaction_id])
+    result = Financial::PlannedTransactions::Execution.destroy(planned_transaction: planned)
+    redirect_back fallback_location: finance_planned_transactions_path, notice: ("Execution removed" if result.success?), alert: (result.error_message unless result.success?)
+  end
+end
