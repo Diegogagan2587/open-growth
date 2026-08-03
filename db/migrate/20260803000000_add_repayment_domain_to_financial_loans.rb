3e3d570aad11be5cff9c05b750ec1fd279c396e8
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-08-03 15:47:34 -0600

    feat(migrations): add columns repayment_basis, final_payment_amount, interest_category, interest_entry on table finacial_loans for tracking loans conditions, and refernce interest rate entry

---
+++ db/migrate/20260803000000_add_repayment_domain_to_financial_loans.rb
@@ -0,0 +1,23 @@
+class AddRepaymentDomainToFinancialLoans < ActiveRecord::Migration[8.0]
+  def up
+    add_column :financial_loans, :repayment_basis, :string
+    add_column :financial_loans, :final_payment_amount, :decimal, precision: 12, scale: 2
+    add_reference :financial_loans, :interest_category, foreign_key: { to_table: :categories }
+    add_reference :financial_loan_installments, :interest_entry, foreign_key: { to_table: :financial_entries }
+
+    execute <<~SQL.squish
+      UPDATE financial_loans
+      SET repayment_basis = CASE
+        WHEN payment_amount IS NOT NULL THEN 'payment_amounts'
+        WHEN interest_rate IS NOT NULL THEN 'annual_rate'
+      END
+    SQL
+  end
+
+  def down
+    remove_reference :financial_loan_installments, :interest_entry, foreign_key: { to_table: :financial_entries }
+    remove_reference :financial_loans, :interest_category, foreign_key: { to_table: :categories }
+    remove_column :financial_loans, :final_payment_amount
+    remove_column :financial_loans, :repayment_basis
+  end
+end
