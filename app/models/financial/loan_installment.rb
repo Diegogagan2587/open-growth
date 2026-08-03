ff55da7438cc703aa7ede918fda96674fd4de6d6
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-08-03 17:24:54 -0600

    feat(financial::LoanInstallment): define LoanInstallment belongs to Financial::Entry as interest entry for automaticall registering  intest entris on loans

--- app/models/financial/loan_installment.rb
+++ app/models/financial/loan_installment.rb
@@ -7,6 +7,7 @@ class Financial::LoanInstallment < ApplicationRecord
   belongs_to :financial_loan, class_name: "Financial::Loan", inverse_of: :installments
   belongs_to :planned_transaction, class_name: "Financial::PlannedTransaction", optional: true
   belongs_to :payment_entry, class_name: "Financial::Entry", optional: true
+  belongs_to :interest_entry, class_name: "Financial::Entry", optional: true
 
   validates :installment_number, numericality: { only_integer: true, greater_than: 0 }, uniqueness: { scope: :financial_loan_id }
   validates :due_date, presence: true
