c8d2615c882c934508b3deac96b5612a97e669a8
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-08-08 11:48:04 -0600

    feat(Financial::EntruRouteCompoent): removes asset and liability endopints, and use only account_endpoint as liaiblity and assets accouns are represented with Account model

--- app/components/financial/entry_route_component.rb
+++ app/components/financial/entry_route_component.rb
@@ -24,16 +24,8 @@ class Financial::EntryRouteComponent < ViewComponent::Base
     account_endpoint("Destination", @entry.routed_destination_account)
   end
 
-  def asset_endpoint(role, asset)
-    endpoint(role, "Asset", asset, helpers.finance_financial_account_path(asset)) if asset
-  end
-
-  def liability_endpoint(role, liability)
-    endpoint(role, "Liability", liability, helpers.finance_financial_liability_path(liability)) if liability
-  end
-
-  def income_event_endpoint(role, income_event)
-    endpoint(role, "Financial plan", income_event, helpers.finance_plan_path(income_event)) if income_event
+  def account_endpoint(role, account)
+    endpoint(role, account.account_group.humanize, account, helpers.finance_account_path(account)) if account
   end
 
   def endpoint(role, kind, record, path)
