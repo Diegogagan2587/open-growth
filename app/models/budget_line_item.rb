f9b3cdd6c973f38296a85e1c64d641a4a7c44a6d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-08-08 11:55:00 -0600

    refact(BudgeLineItem): removes logic from this model and keep it as compatibility layer

--- app/models/budget_line_item.rb
+++ app/models/budget_line_item.rb
@@ -1,15 +1,3 @@
-class BudgetLineItem < ApplicationRecord
-  belongs_to :account
-  belongs_to :budget_period
-  belongs_to :category
-
-  before_validation :set_account, on: :create
-
-  scope :for_account, ->(account) { where(account: account) }
-
-  private
-
-  def set_account
-    self.account ||= Current.account if Current.account
-  end
+# Temporary compatibility adapter. New code uses Financial::BudgetAllocation.
+class BudgetLineItem < Financial::BudgetAllocation
 end
