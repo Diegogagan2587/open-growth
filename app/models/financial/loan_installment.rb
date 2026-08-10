84e8ece8c4b2f02e921af2036154f9eb1b42c629
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-08-09 20:53:10 -0600

    feat(Financial::LoanInstallment): belongs to payment_transaction instead of payment_entry for using canonical model

--- app/models/financial/loan_installment.rb
+++ app/models/financial/loan_installment.rb
@@ -6,9 +6,8 @@ class Financial::LoanInstallment < ApplicationRecord
   belongs_to :account, class_name: "::Account"
   belongs_to :financial_loan, class_name: "Financial::Loan", inverse_of: :installments
   belongs_to :planned_transaction, class_name: "Financial::PlannedTransaction", optional: true
-  belongs_to :payment_entry, class_name: "Financial::Entry", optional: true
+  belongs_to :payment_transaction, class_name: "Financial::Transaction", optional: true
   belongs_to :interest_entry, class_name: "Financial::Entry", optional: true
-
   validates :installment_number, numericality: { only_integer: true, greater_than: 0 }, uniqueness: { scope: :financial_loan_id }
   validates :due_date, presence: true
   validates :expected_amount, numericality: { greater_than: 0 }
