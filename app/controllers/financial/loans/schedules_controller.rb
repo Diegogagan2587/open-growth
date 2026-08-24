539c869f22f67bb5775cf00e2aaf093cee129314
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-08-24 15:56:20 -0600

    feat(Financial::Loans::SchedulesController): generate schedule using first_payemtn_date and pass ids of instalmetns that will be reset

--- app/controllers/financial/loans/schedules_controller.rb
+++ app/controllers/financial/loans/schedules_controller.rb
@@ -1,7 +1,11 @@
 class Financial::Loans::SchedulesController < ApplicationController
   def create
     loan = Financial::Loan.for_account(Current.account).find(params[:loan_id])
-    result = Financial::Loans::RegenerateSchedule.call(loan: loan, start_date: params[:start_date])
+    result = Financial::Loans::RegenerateSchedule.call(
+      loan: loan,
+      first_payment_date: params[:first_payment_date].presence || params[:start_date],
+      reset_manual_installment_ids: params[:reset_manual_installment_ids]
+    )
 
     redirect_to finance_loan_path(loan),
       notice: ("Schedule regenerated and pending plans synchronized" if result.success?),
