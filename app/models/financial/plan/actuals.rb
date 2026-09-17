612e9d3e40dbcb403437c33f7f707b85f0043d2a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-09-16 23:38:22 -0600

    feat(Fiancial::PLan::Actuals#opening_balance): chagne to 0 as we do not calculate global balance on a plan becase it scope is for the curren plan to avoid mislieading balances due to missing registered planned transactions etc

--- app/models/financial/plan/actuals.rb
+++ app/models/financial/plan/actuals.rb
@@ -18,15 +18,7 @@ class Financial::Plan::Actuals
   end
 
   def opening_balance
-    preceding_plans.reduce(0.to_d) do |balance, preceding_plan|
-      if preceding_plan.lifecycle_status == "closed" && preceding_plan.actual_ending_balance_at_close.present?
-        next preceding_plan.actual_ending_balance_at_close.to_d
-      end
-
-      preceding_entries = preceding_plan.financial_entries
-      balance + preceding_entries.where(entry_type: FUNDING_ENTRY_TYPES).sum(:amount).to_d -
-        preceding_entries.where(entry_type: Financial::Entry::EXPENSE_ENTRY_TYPES).sum(:amount).to_d
-    end
+    0.to_d
   end
 
   def ending_balance
